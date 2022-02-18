e06f36c52a08a823a457e49672f71567f9f4638d
Andrey Belianin <belyaninwork@gmail.com>
2022-02-18 23:19:07 +0500

    InsertionSort clean-up

--- Practice.Sorting/InsertionSorting.cs
+++ Practice.Sorting/InsertionSorting.cs
@@ -8,18 +8,16 @@ namespace Practice.Sorting
         {
             for (int i = 1; i < elements.Length; i++)
             {
-                var temp = elements[i];
+                var elementToInsert = elements[i];
                 var j = i;
                 for (; j > 0; j--)
                 {
-                    if (temp.CompareTo(elements[j - 1]) < 0)
-                    {
+                    if (elementToInsert.CompareTo(elements[j - 1]) < 0)
                         elements[j] = elements[j - 1];
-                    }
                     else
                         break;
                 }
-                elements[j] = temp;
+                elements[j] = elementToInsert;
             }
 
             return elements;
